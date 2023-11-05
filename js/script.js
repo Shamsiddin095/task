c27219284c598d82f2daea5578855733ce1295cd
Shamsiddin095 <asqarovshamsiddin455@gmail.com>
2023-11-05 22:37:46 +0500

    task

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function task1() {
+  window.location = "task1.html";
+}
+function task2() {
+  window.location = "task2.html";
+}
+function task3() {
+  window.location = "task3.html";
+}
+function task4() {
+  window.location = "task4.html";
+}
+function task5() {
+  window.location = "task5.html";
+}
+function task6() {
+  window.location = "task6.html";
+}
+function task7() {
+  window.location = "task7.html";
+}
+function task9() {
+  window.location = "task9.html";
+}
+function task10() {
+  window.location = "task10.html";
+}
+function task11() {
+  window.location = "task11.html";
+}
+function task12() {
+  window.location = "task12.html";
+}
+function task13() {
+  window.location = "task13.html";
+}
+function task14() {
+  window.location = "task14.html";
+}
+function task15() {
+  window.location = "task15.html";
+}
+function task16() {
+  window.location = "task16.html";
+}
